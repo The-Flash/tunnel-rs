4491965582daff4c7c74357957271aa031450622
Augustine Smith <absaugustineflash@gmail.com>
2025-12-31 19:02:27 +0000

    add http subcommand

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("tunnel").about("Fast Http Tunnel")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("http").about("Start an http tunnel")
+                .arg(arg!(<PORT> "Port to listen on"))
+                .arg_required_else_help(true)
+        )
+}
+
 fn main() {
-    println!("Starting agent application");
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("http", sub_matches)) => {
+            let port_str = sub_matches.get_one::<String>("PORT").expect("PORT is required");
+            let port_n = match port_str.parse::<i32>() {
+                Ok(number) => number,
+                Err(e) => {
+                    eprintln!("{:?}", e);
+                    return;
+                }
+            };
+            println!("Port {:?}", port_n);
+        },
+        _ => unreachable!()
+    };
 }
