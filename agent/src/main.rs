09c2b33e84cbbfa03bcc70f46792a7e63ea178b0
Augustine Smith <absaugustineflash@gmail.com>
2026-01-01 15:04:37 +0000

    simplify getting of arguments

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,5 +1,5 @@
 use clap::{arg, Arg, Command};
-use tracing::{error, info};
+use tracing::info;
 
 fn cli() -> Command {
     Command::new("tunnel").about("Fast Http Tunnel")
@@ -7,13 +7,16 @@ fn cli() -> Command {
         .arg_required_else_help(true)
         .subcommand(
             Command::new("http").about("Start an http tunnel")
-                .arg(arg!(<PORT> "Port to listen on"))
+                .arg(arg!(<PORT> "Port to listen on")
+                    .value_parser(clap::value_parser!(i32))
+                )
                 .arg_required_else_help(true)
                 .arg(
                     Arg::new("AGENT_PORT")
                     .help("Port the agent is running on")
                     .short('a')
                     .long("agent-port")
+                    .value_parser(clap::value_parser!(i32))
                     .default_value("8000")
                     .default_missing_value("8000")
                 )
@@ -26,25 +29,9 @@ fn main() {
     let matches = cli().get_matches();
     match matches.subcommand() {
         Some(("http", sub_matches)) => {
-            let port_str = sub_matches.get_one::<String>("PORT").expect("PORT is required");
-            let port_n = match port_str.parse::<i32>() {
-                Ok(number) => number,
-                Err(_e) => {
-                    error!("PORT must be a number");
-                    return;
-                }
-            };
-            info!("Starting tunnel on port {:?}...", port_n);
-            let mut agent_port = 8000;
-            if let Some(agent_port_str) = sub_matches.get_one::<String>("AGENT_PORT") {
-                agent_port = match agent_port_str.parse::<i32>() {
-                    Ok(number) => number,
-                    Err(_e) => {
-                        error!("AGENT_PORT must be a number");
-                        return;
-                    }
-                }
-            }
+            let port = sub_matches.get_one::<i32>("PORT").expect("PORT is required");
+            info!("Starting tunnel on port {:?}...", port);
+            let agent_port = sub_matches.get_one::<i32>("AGENT_PORT").expect("AGENT_PORT has a default value");
            info!("Agent is running on port {:?}...", agent_port);
         },
         _ => unreachable!()
